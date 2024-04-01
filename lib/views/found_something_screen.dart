dc549f26237537d51967b026e3061cbe91eb9969
RADO-DAYEF\Rado Dayef <mouraddayef9@gmailcom>
2024-03-31 19:03:52 -0700

    Handled the logic of the found something screen to upload item into collection on firebase called posts The upload button uploads images for category on fire storage into folder called postsImages/theUploaderEmail, category for the item, condition for the item, title and discription for the item Provided the found something screen with new UI removed the categories from the UI and puted them iside a DropdownButton, added a condition for the item new or old, added info for the item like Name and Discription, added container has child icon clicking on that container will open the galery for the user to upload images for the item after the user uploads the images its'll be displayed in MasonryGridView and provided the item from the MasonryGridView with longPress to remove the image from the item images

--- lib/views/found_something_screen.dart
+++ lib/views/found_something_screen.dart
@@ -7,6 +7,7 @@ class FoundSomethingScreen extends GetWidget<FoundSomethingController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        surfaceTintColor: Colors.transparent,
         iconTheme: const IconThemeData(
           color: Colors.black,
           size: 35,
@@ -24,56 +25,394 @@ class FoundSomethingScreen extends GetWidget<FoundSomethingController> {
       ),
       body: Container(
         padding: const EdgeInsets.symmetric(
-          vertical: 40,
-          horizontal: 5,
+          horizontal: 10,
         ),
         width: double.infinity,
         height: MediaQuery.of(context).size.height,
         decoration: const BoxDecoration(
           color: AppColors.blue,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(30),
-            topRight: Radius.circular(30),
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(30),
           ),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              AppStrings.plsSelectTheItemYouFoundText,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: AppColors.white,
-              ),
-            ),
-            const SizedBox(height: 40),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.62,
-              //width: 165,
-              child: GridView.builder(
-                itemCount: controller.categoriesData.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 30,
-
-                  mainAxisSpacing: 20,
+        child: SingleChildScrollView(
+          physics: const ClampingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // const SizedBox(height: 40),
+              // const Text(
+              //   AppStrings.plsSelectTheItemYouFoundText,
+              //   style: TextStyle(
+              //     fontSize: 20,
+              //     fontWeight: FontWeight.bold,
+              //     color: AppColors.white,
+              //   ),
+              // ),
+              // const SizedBox(height: 40),
+              // SizedBox(
+              //   child: GridView.builder(
+              //     physics: const NeverScrollableScrollPhysics(),
+              //     shrinkWrap: true,
+              //     itemCount: controller.categoriesData.length,
+              //     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              //       crossAxisCount: 2,
+              //       crossAxisSpacing: 30,
+              //       mainAxisSpacing: 20,
+              //     ),
+              //     itemBuilder: (BuildContext context, int index) {
+              //       Categories category = controller.categoriesData[index];
+              //       return InkWell(
+              //         onTap: () {
+              //           Get.toNamed(AppStrings.addFoundItemRout, arguments: category);
+              //         },
+              //         child: categoryCard(
+              //           label: category.label,
+              //           img: category.img,
+              //         ),
+              //       );
+              //     },
+              //   ),
+              // ),
+              // const SizedBox(height: 10),
+              Form(
+                key: controller.formState,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Obx(
+                        () {
+                          return MasonryGridView.builder(
+                            physics: NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: controller.images.length,
+                            gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                            ),
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
+                            itemBuilder: (itemBuilder, index) {
+                              return InkWell(
+                                onLongPress: () => controller.imageOnLongPressed(index),
+                                child: Obx(
+                                  () {
+                                    return ClipRRect(
+                                      borderRadius: BorderRadius.circular(15),
+                                      child: Image.file(
+                                        controller.images[index],
+                                        fit: BoxFit.cover,
+                                      ),
+                                    );
+                                  },
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      ),
+                      Obx(
+                        () {
+                          return controller.images.length == 0
+                              ? SizedBox(
+                                  height: 0,
+                                )
+                              : SizedBox(
+                                  height: 15,
+                                );
+                        },
+                      ),
+                      Obx(
+                        () {
+                          return Center(
+                            child: controller.images.length == 0
+                                ? InkWell(
+                                    onTap: controller.pickImages,
+                                    child: Container(
+                                      height: 150,
+                                      width: 150,
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                        color: AppColors.white,
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                      child: Icon(
+                                        Icons.camera_rounded,
+                                        color: AppColors.blue,
+                                        size: 50,
+                                      ),
+                                    ),
+                                  )
+                                : SizedBox(
+                                    height: 0,
+                                  ),
+                          );
+                        },
+                      ),
+                      Divider(height: 50),
+                      Text(
+                        AppStrings.categoryText,
+                        style: TextStyle(
+                          color: AppColors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      DropdownButtonHideUnderline(
+                        child: DropdownButton2<String>(
+                          isExpanded: true,
+                          hint: Row(
+                            children: [
+                              Icon(
+                                Icons.category_rounded,
+                                size: 24,
+                                color: AppColors.white,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Obx(
+                                () {
+                                  return Text(
+                                    controller.categoryBHint.value == AppStrings.emptySign ? AppStrings.selectCategoryText : controller.categoryBHint.value,
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: AppColors.white,
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
+                          items: controller.categoriesList
+                              .map(
+                                (item) => DropdownMenuItem(
+                                  value: item,
+                                  child: Text(
+                                    item,
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: AppColors.blue,
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                          onChanged: (value) {
+                            controller.categoryBHint.value = value!;
+                          },
+                          buttonStyleData: ButtonStyleData(
+                            height: 25,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(14),
+                            ),
+                          ),
+                          iconStyleData: IconStyleData(
+                            icon: Icon(
+                              Icons.keyboard_arrow_right_rounded,
+                              size: 24,
+                              color: AppColors.white,
+                            ),
+                            openMenuIcon: Icon(
+                              Icons.keyboard_arrow_down_rounded,
+                              size: 24,
+                              color: AppColors.white,
+                            ),
+                            iconSize: 20,
+                            iconEnabledColor: AppColors.white,
+                            iconDisabledColor: AppColors.white,
+                          ),
+                          dropdownStyleData: DropdownStyleData(
+                            maxHeight: 200,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(14),
+                              color: AppColors.white,
+                            ),
+                            scrollbarTheme: ScrollbarThemeData(
+                              radius: Radius.circular(15),
+                            ),
+                          ),
+                          menuItemStyleData: MenuItemStyleData(
+                            padding: EdgeInsets.all(10),
+                          ),
+                        ),
+                      ),
+                      Divider(height: 50),
+                      Text(
+                        AppStrings.conditionText,
+                        style: TextStyle(
+                          color: AppColors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Wrap(
+                        spacing: 8,
+                        runSpacing: 4,
+                        children: [
+                          Obx(
+                            () {
+                              return InkWell(
+                                onTap: controller.newButtonOnClick,
+                                child: Container(
+                                  height: 35,
+                                  width: 70,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    color: controller.newSelected.value == false ? AppColors.white : AppColors.blue,
+                                    borderRadius: BorderRadius.circular(15),
+                                    border: Border.all(
+                                      color: controller.newSelected.value == false ? AppColors.blue : AppColors.white,
+                                    ),
+                                  ),
+                                  child: Text(
+                                    AppStrings.newText,
+                                    style: TextStyle(
+                                      color: controller.newSelected.value == false ? AppColors.blue : AppColors.white,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                          Obx(
+                            () {
+                              return InkWell(
+                                onTap: controller.usedButtonOnClick,
+                                child: Container(
+                                  height: 35,
+                                  width: 70,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    color: controller.usedSelected.value == false ? AppColors.white : AppColors.blue,
+                                    borderRadius: BorderRadius.circular(15),
+                                    border: Border.all(
+                                      color: controller.usedSelected.value == false ? AppColors.blue : AppColors.white,
+                                    ),
+                                  ),
+                                  child: Text(
+                                    AppStrings.usedText,
+                                    style: TextStyle(
+                                      color: controller.usedSelected.value == false ? AppColors.blue : AppColors.white,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                      Divider(height: 50),
+                      Text(
+                        AppStrings.infoText,
+                        style: TextStyle(
+                          color: AppColors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      AuthInputField(
+                        color: AppColors.white,
+                        onSaved: (value) {
+                          controller.title.value = value!;
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return AppStrings.titleEmptyValidate;
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.text,
+                        labelName: AppStrings.titleText,
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      AuthInputField(
+                        color: AppColors.white,
+                        maxLines: 10,
+                        onSaved: (value) {
+                          controller.description.value = value!;
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return AppStrings.descriptionEmptyValidate;
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.text,
+                        labelName: AppStrings.descriptionText,
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Center(
+                        child: Obx(
+                          () {
+                            return controller.isLoading.value == false
+                                ? InkWell(
+                                    onTap: controller.uploadOnClick,
+                                    child: Container(
+                                      height: 50,
+                                      width: 150,
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                        color: AppColors.white,
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                      child: Text(
+                                        AppStrings.uploadText,
+                                        style: TextStyle(
+                                          color: AppColors.blue,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                : CircleAvatar(
+                                    radius: 25,
+                                    backgroundColor: AppColors.blue,
+                                    child: Container(
+                                      padding: EdgeInsets.all(10),
+                                      child: CircularProgressIndicator(
+                                        color: AppColors.white,
+                                      ),
+                                    ),
+                                  );
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                    ],
+                  ),
                 ),
-                itemBuilder: (BuildContext context, int index) {
-                  Categories category = controller.categoriesData[index];
-                  return InkWell(
-                    onTap: () {
-                      Get.toNamed(AppStrings.addFoundItemRout, arguments: category);
-                    },
-                    child: categoryCard(
-                      label: category.label,
-                      img: category.img,
-                    ),
-                  );
-                },
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
